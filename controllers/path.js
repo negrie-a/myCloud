bf2191120f73a178f636a327e5bbc1fccc0c513c
Negrier Aurelien <negrier.aurelien@gmail.com>
2017-02-18 00:35:23 +0100

    [FEATURE]: Add logout and create account

--- controllers/path.js
+++ controllers/path.js
@@ -7,77 +7,77 @@ var diskspace = require('diskspace');
 var exec = require('child_process').exec;
 
 var getContents = function (req, res) {
-  var pathFiles = [global.rootPath, "data", req.user.id, req.body.path].createPath("/");
+    var pathFiles = [global.rootPath, "data", req.user.id, req.body.path].createPath("/");
 
-  fs.readdir(pathFiles, function(err, files) {
-      if (err != null) {
-        console.log(err);
-        return res.status(404).send("Can not get content on the server");
-      }
-      var filesArray = [];
-      var folderArray = [];
-
-      for (var i = 0 ; i < files.length ; i++) {
-        var object = fs.lstatSync(pathFiles + "/" + files[i]);
-        if (object.isFile()) {
-
-          filesArray.push({
-            name: files[i],
-            size: object.size,
-            path: req.body.path
-          });
+    fs.readdir(pathFiles, function(err, files) {
+        if (err != null) {
+            console.log(err);
+            return res.status(404).send("Can not get content on the server");
         }
-        else {
-          if (files[i] != ".diminutive" && files[i] !== ".DS_Store") // don't send small picture hide in .diminutive
-            folderArray.push(files[i]);
+        var filesArray = [];
+        var folderArray = [];
+
+        for (var i = 0 ; i < files.length ; i++) {
+            var object = fs.lstatSync(pathFiles + "/" + files[i]);
+            if (object.isFile()) {
+
+                filesArray.push({
+                    name: files[i],
+                    size: object.size,
+                    path: req.body.path
+                });
+            }
+            else {
+                if (files[i] != ".diminutive" && files[i] !== ".DS_Store") // don't send small picture hide in .diminutive
+                folderArray.push(files[i]);
+            }
         }
-      }
-      return res.status(200).send({files : filesArray, folders: folderArray, path: req.body.path});
-  })
+        return res.status(200).send({files : filesArray, folders: folderArray, path: req.body.path});
+    })
 }
 
 var getTreeRepository = function(req, res) {
-  var walker  = walk.walk([global.rootPath, "data", req.user.id].createPath("/"), { followLinks: false });
-  var files = []
-  walker.on('directory', function(root, stat, next) {
-      if (stat.name !== ".diminutive" && stat.name !== ".DS_Store") {
-        var path = root.replace([global.rootPath, "data", req.user.id].createPath("/"), '')
-        var filePath = path + '/' + stat.name
-        files.push(filePath.slice(1));
-      }
-      next();
-  });
+    var walker  = walk.walk([global.rootPath, "data", req.user.id].createPath("/"), { followLinks: false });
+    var files = []
+    walker.on('directory', function(root, stat, next) {
+        if (stat.name !== ".diminutive" && stat.name !== ".DS_Store") {
+            var path = root.replace([global.rootPath, "data", req.user.id].createPath("/"), '')
+            var filePath = path + '/' + stat.name
+            files.push(filePath.slice(1));
+        }
+        next();
+    });
 
-  walker.on("errors", function (root, nodeStatsArray, next) {
-    console.log("[ERROR] : getRepositoryTree");
-    next();
-  });
+    walker.on("errors", function (root, nodeStatsArray, next) {
+        console.log("[ERROR] : getRepositoryTree");
+        next();
+    });
 
-  walker.on('end', function() {
-      res.status(200).send(files)
-  });
+    walker.on('end', function() {
+        res.status(200).send(files)
+    });
 }
 
 var getStatsRepository = function(req, res) {
-    var path = [global.rootPath, "data", "2"].createPath("/");
+    var path = [global.rootPath, "data", req.user.id].createPath("/");
     var walker  = walk.walk(path, { followLinks: false });
     const diskStats = {
-      totalSizeUsed: 0,
-      totalSizeServer: 0,
-      totalSizeFree: 0,
-      pourcentage: 0,
-      totalFolders: 0,
-      totalFiles: 0,
-      totalPictures: 0,
-      totalMovies: 0,
-      sizeFolders: 0,
-      sizePictures: 0,
-      sizeMovies: 0,
+        totalSizeUsed: 0,
+        totalSizeServer: 0,
+        totalSizeFree: 0,
+        pourcentage: 0,
+        totalFolders: 0,
+        totalFiles: 0,
+        totalPictures: 0,
+        totalMovies: 0,
+        sizeFolders: 0,
+        sizePictures: 0,
+        sizeMovies: 0,
     }
 
     const disk = exec('df -m | grep /dev/disk1', (err, stdout, stderr) => {
         if (err) {
-          console.log(err)
+            console.log(err)
         }''
         var arrayDiskStat = stdout.replace(/\s+/g,' ').split(' ');
         diskStats.totalSizeFree = Number(arrayDiskStat[3]) * 1000000;
@@ -87,66 +87,66 @@ var getStatsRepository = function(req, res) {
 
     var files = fs.readdirSync(path)
     files.forEach(function(file) {
-      if (file !== ".diminutive" && file !== ".DS_Store") {
-        var stats = fs.lstatSync([path, file].createPath("/"))
-        if (stats.isFile()) {
-          if (file.isImage()) {
-            diskStats.totalPictures += 1
-            diskStats.sizePictures += stats.size
-          }
-          else if (file.isMovie()) {
-            diskStats.totalMovies += 1
-            diskStats.sizeMovies += stats.size
-          }
-          diskStats.totalSizeUsed += stats.size
-          diskStats.totalFiles += 1
+        if (file !== ".diminutive" && file !== ".DS_Store") {
+            var stats = fs.lstatSync([path, file].createPath("/"))
+            if (stats.isFile()) {
+                if (file.isImage()) {
+                    diskStats.totalPictures += 1
+                    diskStats.sizePictures += stats.size
+                }
+                else if (file.isMovie()) {
+                    diskStats.totalMovies += 1
+                    diskStats.sizeMovies += stats.size
+                }
+                diskStats.totalSizeUsed += stats.size
+                diskStats.totalFiles += 1
+            }
+            else if (stats.isDirectory()) {
+                diskStats.totalFolders += 1
+                diskStats.sizeFolders += stats.size
+            }
         }
-        else if (stats.isDirectory()) {
-          diskStats.totalFolders += 1
-          diskStats.sizeFolders += stats.size
-        }
-      }
     })
 
     walker.on('directory', function(root, stats, next) {
-          var pathFolder = [root, stats.name].createPath("/")
-          if (stats.name !== ".diminutive" && stats.name !== ".DS_Store") {
+        var pathFolder = [root, stats.name].createPath("/")
+        if (stats.name !== ".diminutive" && stats.name !== ".DS_Store") {
             fs.readdir(pathFolder, function(err, files) {
                 if (err) {
-                  console.log(err)
+                    console.log(err)
                 }
                 files.forEach(function(file) {
-                  var stats = fs.lstatSync([pathFolder, file].createPath("/"))
-                  if (file !== ".diminutive" && file !== ".DS_Store") {
-                    if (stats.isFile()) {
-                      if (file.isImage()) {
-                        diskStats.totalPictures += 1
-                        diskStats.sizePictures += stats.size
-                      }
-                      else if (file.isMovie()) {
-                        diskStats.totalMovies += 1
-                        diskStats.sizeMovies += stats.size
-                      }
-                      diskStats.totalSizeUsed += stats.size
-                      diskStats.totalFiles += 1
+                    var stats = fs.lstatSync([pathFolder, file].createPath("/"))
+                    if (file !== ".diminutive" && file !== ".DS_Store") {
+                        if (stats.isFile()) {
+                            if (file.isImage()) {
+                                diskStats.totalPictures += 1
+                                diskStats.sizePictures += stats.size
+                            }
+                            else if (file.isMovie()) {
+                                diskStats.totalMovies += 1
+                                diskStats.sizeMovies += stats.size
+                            }
+                            diskStats.totalSizeUsed += stats.size
+                            diskStats.totalFiles += 1
+                        }
+                        else if (stats.isDirectory()) {
+                            diskStats.totalFolders += 1
+                            diskStats.sizeFolders += stats.size
+                        }
                     }
-                    else if (stats.isDirectory()) {
-                      diskStats.totalFolders += 1
-                      diskStats.sizeFolders += stats.size
-                    }
-                  }
                 })
                 next();
             })
         }
         else {
-          next();
+            next();
         }
     });
 
     walker.on("errors", function (root, nodeStatsArray, next) {
-      console.log("[ERROR] : getStatsTree");
-      next();
+        console.log("[ERROR] : getStatsTree");
+        next();
     });
 
     walker.on('end', function() {
@@ -155,24 +155,24 @@ var getStatsRepository = function(req, res) {
 }
 
 module.exports = {
-  '/contents': {
+    '/contents': {
         post: {
             action: getContents,
             level: 'member'
         }
     },
 
-  '/treeRepository': {
+    '/treeRepository': {
         get: {
-          action: getTreeRepository,
-          level: 'public'
+            action: getTreeRepository,
+            level: 'public'
         }
     },
 
-  '/statsRepository': {
-    get: {
-      action: getStatsRepository,
-      level: 'public'
+    '/statsRepository': {
+        get: {
+            action: getStatsRepository,
+            level: 'public'
+        }
     }
-  }
 }
--- controllers/user.js
+++ controllers/user.js
@@ -4,61 +4,140 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 
 var connection = function (req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      console.log(err)
-      return next(err);
-    }
-    if (!user) {
-      console.log(info);
-      return res.status(401).send(info.message);
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
-
-      const disk = exec('df -m | grep /dev/disk1', (err, stdout, stderr) => {
+    passport.authenticate('local', function(err, user, info) {
         if (err) {
-          console.log(err)
+            console.log(err)
+            return next(err);
         }
-        var arrayDiskStat = stdout.replace(/\s+/g,' ').split(' ');
-        var totalSizeFree = Number(arrayDiskStat[3]) * 1000000;
-        console.log(totalSizeFree)
-        if (user.pathProfilPicture !== null) {
-          var pathProfilPicture = [global.rootPath, "profil_picture", user.pathProfilPicture].createPath("/")
-          var stat = fs.statSync(pathProfilPicture)
-
-          var options = {
-            headers: {
-              'x-timestamp': Date.now(),
-              'x-sent': true,
-              'firstName': user.firstName,
-              'lastName': user.lastName,
-              'totalSizeFree': totalSizeFree
+        if (!user) {
+            console.log(info);
+            return res.status(401).send(info.message);
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                console.log(err);
+                return next(err);
             }
-          };
-          return res.status(200).sendFile(pathProfilPicture, options);
+
+            const disk = exec('df -m | grep /dev/disk1', (err, stdout, stderr) => {
+                if (err) {
+                    console.log(err)
+                }
+                var arrayDiskStat = stdout.replace(/\s+/g,' ').split(' ');
+                var totalSizeFree = Number(arrayDiskStat[3]) * 1000000;
+                console.log(totalSizeFree)
+                if (user.pathProfilPicture !== null) {
+                    var pathProfilPicture = [global.rootPath, "profil_picture", user.pathProfilPicture].createPath("/")
+                    var stat = fs.statSync(pathProfilPicture)
+
+                    var options = {
+                        headers: {
+                            'x-timestamp': Date.now(),
+                            'x-sent': true,
+                            'firstName': user.firstName,
+                            'lastName': user.lastName,
+                            'totalSizeFree': totalSizeFree
+                        }
+                    };
+                    return res.status(200).sendFile(pathProfilPicture, options);
+                }
+                else {
+                    res.header({
+                        'firstName': user.firstName,
+                        'lastName': user.lastName,
+                        'totalSizeFree' : totalSizeFree
+                    });
+                    return res.status(200).end();
+                }
+            })
+        });
+    })(req, res, next);
+}
+
+var logout = function(req, res) {
+    req.logout();
+    return res.status(200).end();
+}
+
+var subscribe = function(req, res) {
+    var User = tmS.getModel('User');
+    User.findOne({where: {"email": req.body.email}})
+    .then(function(user) {
+        if (user !== null) {
+            return res.status(404).send("Email already used");
         }
-        else {
-          res.header({
-            'firstName': user.firstName,
-            'lastName': user.lastName,
-            'totalSizeFree' : totalSizeFree
-          });
-          return res.status(200).end();
-        }
-      })
-    });
-  })(req, res, next);
+        User.create(req.body)
+        .then(function(user) {
+            fs.mkdir([global.rootPath, "data", user.id].createPath("/"), function(err) {
+                if (err) {
+                    console.log(err)
+                    return res.status(404).send("Can not create folder")
+                }
+                return res.status(200).send("Account created");
+            })
+        })
+        .catch(function(err) {
+            console.log(err)
+            return res.status(404).send("Invalid information");
+        })
+    })
+}
+
+var uploadProfilPicture = function(req, res) {
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (name, file, filename) {
+        var bufferList = []
+        var bufferListSize = 0
+        var pathFile = [global.rootPath, "profil_picture", filename].createPath("/");
+
+        file.on('data', (chunk) => {
+            bufferList.push(chunk)
+            bufferListSize += chunk.length
+        });
+
+        file.on('close', function () {
+            console.log("CLOSE")
+        });
+
+        file.on('end', function () {
+            const data = Buffer.concat(bufferList, bufferListSize)
+            fs.writeFile(pathFile, data, function (err) {
+                if (err) {
+                    console.log(err)
+                    return res.status(404).send("Can not append data in file ");
+                }
+                return res.status(200).send("Success upload")
+            })
+        })
+    })
 }
 
 module.exports = {
-  '/login': {
-    post: {
-      action: connection,
-      level: 'public'
+    '/login': {
+        post: {
+            action: connection,
+            level: 'public'
+        }
+    },
+
+    '/logout': {
+        get: {
+            action: logout,
+            level: 'member'
+        }
+    },
+
+    '/subscribe': {
+        post: {
+            action: subscribe,
+            level: 'public'
+        }
+    },
+
+    "/uploadProfilPicture": {
+        post: {
+            action: uploadProfilPicture,
+            level: 'public'
+        }
     }
-  }
 }
